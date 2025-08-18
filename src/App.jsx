8cb15a291651f9030d76a5d8a67489ac71b09826
telaprolulasya <2300031910cseh@gmail.com>
2025-08-18 20:21:28 +0530

    frontend V3.0 commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    <BrowserRouter basename="/ecommerce-frontend">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
